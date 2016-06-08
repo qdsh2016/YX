c4846f35b1fa5716c68bf9d13b5e25280fc6e879
yxzoro <yangxing20107@qq.com>
2016-06-08 16:24:14 +0800

    Update urls.py

--- student_sys/project/X/urls.py
+++ student_sys/project/X/urls.py
@@ -18,20 +18,20 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'start', views.start,name='start'),
-	url(r'login', views.login,name='login'),
-	url(r'signin', views.signin,name='signin'),	
-    url(r'ok_insign', views.signin_ok,name='signin_ok'),
+    url(r'^admin/$', admin.site.urls),
+	url(r'start$', views.start,name='start'),
+	url(r'login$', views.login,name='login'),
+	url(r'signin$', views.signin,name='signin'),	
+    url(r'ok_insign$', views.signin_ok,name='signin_ok'),
 
-    url(r'search', views.search,name='search'),    
-    url(r'student', views.student,name='student'),    
-    url(r'score', views.score,name='score'),    
-    url(r'delete', views.delete,name='delete'),  
-    url(r'yyy', views.yyy,name='ok_add'), 
-    url(r'add', views.add,name='add'),  
-    url(r'id_check', views.id_check,name='id_check'), 
-    url(r'update', views.update,name='update'),  
-    url(r'kkk', views.ko_update,name='ko_update'),  
+    url(r'search$', views.search,name='search'),    
+    url(r'student$', views.student,name='student'),    
+    url(r'score$', views.score,name='score'),    
+    url(r'delete$', views.delete,name='delete'),  
+    url(r'yyy$', views.yyy,name='ok_add'), 
+    url(r'add$', views.add,name='add'),  
+    url(r'id_check$', views.id_check,name='id_check'), 
+    url(r'update$', views.update,name='update'),  
+    url(r'kkk$', views.ko_update,name='ko_update'),  
     
 ]
